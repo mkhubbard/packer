2ec083bb6a0f523280264056687b8c0a71309bb7
Michael Hubbard <mkhubbard@gmail.com>
2015-05-06 21:06:41 -0400

    Fixed guest extensions installation and other issues.

--- CentOS-7-x86_64/README.md
+++ CentOS-7-x86_64/README.md
@@ -1,4 +1,7 @@
-Packer
-======
+CentOS 7.0 x86-64
+=================
 
-Packer template configurations.
+Packer template configuration for building a CentOS 7.0 x86-64 fragment for VirtualBox including guest extenstions.
+
+Based on: https://github.com/tacahilo/packer-centos-7
+
--- CentOS-7-x86_64/scripts/virtualbox.sh
+++ CentOS-7-x86_64/scripts/virtualbox.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
-/mnt/VBoxLinuxAdditions.run
-umount /mnt
-rm -rf VBoxGuestAdditions.iso
+mkdir -p /mnt/virtualbox
+mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
+sh /mnt/virtualbox/VBoxLinuxAdditions.run
+ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+umount /mnt/virtualbox
+rm -rf /home/vagrant/VBoxGuest*.iso
